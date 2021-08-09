58eead6f4dfd9329ab8bc17ea3fd44b3b5edf4dc
Leonardo <leonardo.gomez.1297@gmail.com>
2021-08-09 15:54:59 +0000

    Ajustes view

--- linserman/views.py
+++ linserman/views.py
@@ -33,7 +33,7 @@ def login(request):
         return Response({'data':"Contraseña no válida",'code':400},status=status.HTTP_400_BAD_REQUEST)
 
     token, created = Token.objects.get_or_create(user=user)
-    
+
     return Response({'data':{
         'token':token.key,
         'nombres':user.nombres,
